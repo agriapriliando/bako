0339b3645e3f8d81191831532866138a275911e1
agriapriliando <agripriliando@gmail.com>
2024-05-14 18:58:56 +0700

    edit navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -32,7 +32,7 @@
                 <div class="row align-items-center">
                     <div class="col-lg-3 col-md-3 col-6">
                         <!-- Start Header Logo -->
-                        <a class="navbar-brand" href="index.html">
+                        <a class="navbar-brand" href="{{ url('') }}">
                             <img src="{{ asset('assets/images/logo/logo_tulisan.png') }}" alt="Logo">
                         </a>
                         <!-- End Header Logo -->
@@ -80,7 +80,7 @@
                     <div>
                         <nav class="navbar navbar-expand-lg navbar-light bg-light">
                             <div class="container-fluid">
-                                <a class="navbar-brand" href="#">Sembako</a>
+                                <a id="jam2" class="d-block d-md-none" href="#"></a>
                                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false"
                                     aria-label="Toggle navigation">
                                     <span class="navbar-toggler-icon"></span>
@@ -88,10 +88,28 @@
                                 <div class="collapse navbar-collapse ms-3" id="navbarText">
                                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                                         <li class="nav-item">
-                                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                            <a class="nav-link active" aria-current="page" href="{{ url('') }}">Home</a>
                                         </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="#">Features</a>
+                                        <li class="nav-item dropdown">
+                                            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">
+                                                Kategori
+                                            </a>
+                                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                                @foreach ($categories as $item)
+                                                    <li class="nav-item dropend">
+                                                        <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">
+                                                            {{ $item->nama }}
+                                                        </a>
+                                                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                                            @foreach ($items as $i)
+                                                                @if ($i->category_id == $item->id)
+                                                                    <li><a class="dropdown-item" href="{{ url('grafikbarang/' . $i->id) }}">{{ $i->nama }}</a></li>
+                                                                @endif
+                                                            @endforeach
+                                                        </ul>
+                                                    </li>
+                                                @endforeach
+                                            </ul>
                                         </li>
                                         <li class="nav-item dropdown">
                                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -102,31 +120,12 @@
                                                 <li><a class="dropdown-item" href="#">Tahunan</a></li>
                                             </ul>
                                         </li>
-                                        <li class="nav-item dropdown">
-                                            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">
-                                                Kategori
-                                            </a>
-                                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                                @foreach ($categories as $item)
-                                                    {{-- <li><a class="dropdown-item" href="#">Action</a></li> --}}
-                                                    <li class="nav-item dropend">
-                                                        <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">
-                                                            {{ $item->nama }}
-                                                        </a>
-                                                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                                            @foreach ($items as $i)
-                                                                @if ($i->category_id == $item->id)
-                                                                    <li><a class="dropdown-item" href="#">{{ $i->nama }}</a></li>
-                                                                @endif
-                                                            @endforeach
-                                                        </ul>
-                                                    </li>
-                                                @endforeach
-                                            </ul>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">Tentang</a>
                                         </li>
                                     </ul>
-                                    <span class="navbar-text">
-                                        Navbar text with an inline element
+                                    <span class="navbar-text bg-primary px-3 rounded text-white" id="jam">
+                                        {{ Carbon\Carbon::now()->translatedFormat('l, j F Y H:i') . ' WIB' }}
                                     </span>
                                 </div>
                             </div>
@@ -137,4 +136,70 @@
         </div>
         <!-- End Header Bottom -->
     </header>
+    <script type="text/javascript">
+        <!--
+        function showTime() {
+            var months = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
+            var myDays = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jum&#39;at', 'Sabtu'];
+            var date = new Date();
+            var day = date.getDate();
+            var month = date.getMonth();
+            var thisDay = date.getDay(),
+                thisDay = myDays[thisDay];
+            var yy = date.getYear();
+            var year = (yy < 1000) ? yy + 1900 : yy;
+            var tanggal = thisDay + ', ' + day + ' ' + months[month] + ' ' + year;
+
+            var a_p = "";
+            var today = new Date();
+            var curr_hour = today.getHours();
+            var curr_minute = today.getMinutes();
+            var curr_second = today.getSeconds();
+            // if (curr_hour < 12) {
+            //     a_p = "AM";
+            // } else {
+            //     a_p = "PM";
+            // }
+            // if (curr_hour == 0) {
+            //     curr_hour = 12;
+            // }
+            // if (curr_hour > 12) {
+            //     curr_hour = curr_hour - 12;
+            // }
+            // curr_hour = checkTime(curr_hour);
+            // curr_minute = checkTime(curr_minute);
+            // curr_second = checkTime(curr_second);
+            var jam = curr_hour + ":" + curr_minute + ":" + curr_second + " WIB";
+            document.getElementById('jam').innerHTML = tanggal + ' ' + jam;
+            document.getElementById('jam2').innerHTML = tanggal + ' ' + jam;
+        }
+
+        function checkTime(i) {
+            if (i < 10) {
+                i = "0" + i;
+            }
+            return i;
+        }
+        setInterval(showTime, 500);
+        //
+        -->
+    </script>
+
+    <!-- Menampilkan Hari, Bulan dan Tahun -->
+    <br>
+    <script type='text/javascript'>
+        <!--
+        var months = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
+        var myDays = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jum&#39;at', 'Sabtu'];
+        var date = new Date();
+        var day = date.getDate();
+        var month = date.getMonth();
+        var thisDay = date.getDay(),
+            thisDay = myDays[thisDay];
+        var yy = date.getYear();
+        var year = (yy < 1000) ? yy + 1900 : yy;
+        console.log(thisDay + ', ' + day + ' ' + months[month] + ' ' + year);
+        //
+        -->
+    </script>
 </div>
