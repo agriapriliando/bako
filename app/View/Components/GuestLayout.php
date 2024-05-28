f3d421ecdd92883162762e1dc87bb913872da67d
agriapriliando <agripriliando@gmail.com>
2024-05-28 18:37:39 +0700

    add SEo on guest

--- app/View/Components/GuestLayout.php
+++ app/View/Components/GuestLayout.php
@@ -2,6 +2,7 @@
 
 namespace App\View\Components;
 
+use App\Models\Setting;
 use Illuminate\View\Component;
 use Illuminate\View\View;
 
@@ -12,6 +13,9 @@ class GuestLayout extends Component
      */
     public function render(): View
     {
-        return view('layouts.guest');
+        $seojudul = Setting::find(12);
+        $seoisi = Setting::find(13);
+        $seoimg = Setting::find(14);
+        return view('layouts.guest', compact('seojudul', 'seoisi', 'seoimg'));
     }
 }
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,8 +6,31 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     {{-- <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> --}}
-    <title>Harga Sembako</title>
-    <meta name="description" content="" />
+
+    {{-- SEO Metatag --}}
+    <!-- Primary Meta Tags -->
+    <title>{{ $seojudul->isi }}</title>
+    <meta name="title" content="{{ $seojudul->isi }}" />
+    <meta name="description" content="{{ $seoisi->isi }}" />
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="{{ url('') }}" />
+    <meta property="og:title" content="{{ $seojudul->isi }}" />
+    <meta property="og:description" content="{{ $seoisi->isi }}" />
+    <meta property="og:image" content="{{ asset('storage/images/setting/' . $seoimg->isi) }}" />
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="{{ url('') }}" />
+    <meta property="twitter:title" content="{{ $seojudul->isi }}" />
+    <meta property="twitter:description" content="{{ $seoisi->isi }}" />
+    <meta property="twitter:image" content="{{ asset('storage/images/setting/' . $seoimg->isi) }}" />
+
+    <!-- Meta Tags Generated with https://metatags.io -->
+    {{-- SEO Metatag --}}
+
+
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('') }}assets/images/favicon.svg" />
 
     <!-- ========================= CSS here ========================= -->
