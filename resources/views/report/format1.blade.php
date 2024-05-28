6698ed1171f5a23d4e4cfea2e9c84efc3f153cfc
agriapriliando <agripriliando@gmail.com>
2024-05-28 13:08:08 +0700

    remove vite css

--- resources/views/report/format1.blade.php
+++ resources/views/report/format1.blade.php
@@ -6,7 +6,6 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laporan</title>
-    @vite('resources/js/app.js')
 </head>
 <style>
     table.customTable {
